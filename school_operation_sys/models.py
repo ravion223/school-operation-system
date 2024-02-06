b0c2fcd623f4ab980198e9bdd77b8b42099b509f
ravion223 <tablettaoson2016@gmail.com>
2024-02-06 21:09:01 +0200

    created models and db

--- school_operation_sys/models.py
+++ school_operation_sys/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length = 63)
+
+
+class Teacher(models.Model):
+    teachers_name = models.CharField(max_length = 63)
+    teachers_surname = models.CharField(max_length = 63)
+    subject = models.ForeignKey(Subject, on_delete = models.CASCADE, null = True)
+
+
+class Class(models.Model):
+    class_name = models.CharField(max_length = 63)
+
+
+class Student(models.Model):
+    students_name = models.CharField(max_length = 63)
+    students_surname = models.CharField(max_length = 63)
+    classes = models.ForeignKey(Class, on_delete = models.CASCADE, null = True)
